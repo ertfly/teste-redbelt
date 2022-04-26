a514f35378251a1ac7aad0e40e62f870148a2b33
Eric Teixeira <ertfly@gmail.com>
2022-04-26 02:39:57 -0300

    ajustes

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -10,7 +10,7 @@ class User extends Model
 
     protected $fillable = [
         'name',
-        'email',
+        'username',
         'pass',
     ];
 }
--- server/database/migrations/2021_07_27_0244_create_users_table.php
+++ server/database/migrations/2021_07_27_0244_create_users_table.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Libraries\Strings;
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\String_;
 
 class CreateUsersTable extends Migration
 {
@@ -16,12 +19,17 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 250)->index();
-            $table->string('email', 250)->index();
+            $table->string('username', 50)->index();
             $table->string('pass', 64);
             $table->timestamps();
         });
 
-
+        $user = new User([
+            'name' => 'Administrador',
+            'username' => 'admin',
+            'pass' => Strings::password('admin')
+        ]);
+        $user->save();
     }
 
     /**
