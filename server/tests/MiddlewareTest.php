85f44ffa5143d71eeefe591d67b464bbaf7e4098
Eric Teixeira <ertfly@gmail.com>
2022-04-26 03:38:52 -0300

    ajustes

--- server/tests/MiddlewareTest.php
+++ server/tests/MiddlewareTest.php
@@ -6,15 +6,31 @@ class SessionTest extends TestCase
 {
     public function testShouldBeRouteNotFound()
     {
-        $this->get('/route-not-found-test',['token'=>'123']);
+        $this->get('/route-not-found-test');
 
         $data = $this->response->getContent();
 
         $this->assertEquals(json_encode([
-            'response'=>[
+            'response' => [
                 'action' => 1,
                 'msg' => 'Ocorreu um erro, favor tentar novamente mais tarde.',
                 'internal' => 'Rota inválida'
+            ],
+            'data' => null,
+        ]), $this->response->getContent());
+    }
+
+    public function testShouldBeMethodNotAllowed()
+    {
+        $this->get('/token');
+
+        $data = $this->response->getContent();
+
+        $this->assertEquals(json_encode([
+            'response' => [
+                'action' => 1,
+                'msg' => 'Ocorreu um erro, favor tentar novamente mais tarde.',
+                'internal' => 'Método do endpoint não permitido'
             ],
             'data' => null,
         ]), $this->response->getContent());
