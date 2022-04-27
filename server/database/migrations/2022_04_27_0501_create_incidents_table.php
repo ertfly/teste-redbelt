652797749c1b066662d9720c7d1efa45db0bcc6a
Eric Teixeira <ertfly@gmail.com>
2022-04-27 05:06:40 -0300

    ajustes

---
+++ server/database/migrations/2022_04_27_0501_create_incidents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncidentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250)->index();
+            $table->text('description');
+            $table->integer('criticality_id')->index();
+            $table->integer('type_id')->index();
+            $table->integer('status_id')->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+}
