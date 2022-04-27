0271d39215e88e968d3037fe5d7a6a65308de59b
Eric Teixeira <ertfly@gmail.com>
2022-04-27 00:09:46 -0300

    ajustes

--- node/src/Components/Register/User.js
+++ node/src/Components/Register/User.js
@@ -16,7 +16,58 @@ function UserList() {
     return (
         <>
             <HeaderIn breadcrumb={breadcrumb} />
-            Usuários lista
+            <div className="container-fluid">
+                <div className="card">
+                    <div className="card-body">
+                        <div className="d-flex justify-content-between">
+                            <div>
+                                <h2 className="card-title">Usuários</h2>
+                                <p className="card-text">Gestão dos dados do usuário</p>
+                            </div>
+                            <div>
+                                <a href="/" className="btn btn-primary">
+                                    <i className="fa fa-plus fa-white"></i> Adicionar
+                                </a>
+                            </div>
+                        </div>
+                        <ul className="nav nav-tabs mt-4" id="myTab" role="tablist">
+                            <li className="nav-item">
+                                <a className="nav-link active" href="/">Consulta</a>
+                            </li>
+                        </ul>
+                        <div className="tabcontent-border">
+                            <div className="table-responsive">
+                                <table className="table table-hover table-striped">
+                                    <thead>
+                                        <tr>
+                                            <th className="text-left">Nome</th>
+                                            <th className="text-left">Usuário</th>
+                                            <th className="text-center">Perfis</th>
+                                            <th className="text-right">Opções</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>asdasd</td>
+                                            <td>asdasd</td>
+                                            <td className="text-center">
+                                                asdasd
+                                            </td>
+                                            <td className="text-right">
+                                                <a href="/" className="btn btn-primary btn-sm" title="Editar registro"><i className="fa fa-pencil fa-white"></i></a>
+                                                <button type="button" className="btn btn-danger btn-sm" title="Excluir registro"><i className="fa fa-trash fa-white"></i></button>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="text-center" colspan="20">Nenhum registro encontrado!</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
