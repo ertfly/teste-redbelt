573e2e1cd6ffa32047aab830f07db8c130849cb8
Eric Teixeira <ertfly@gmail.com>
2022-04-28 01:31:17 -0300

    ajustes

--- server/tests/IncidentTest.php
+++ server/tests/IncidentTest.php
@@ -127,54 +127,5 @@ class IncidentTest extends TestCase
         ]);
     }
 
-    public function testShouldBeNotUpdateIncidentWhyNotLogin()
-    {
-        $this->json('POST', '/token', [], [
-            'Content-Type' => 'application/json'
-        ]);
-        $responseToken = json_decode($this->response->getContent(), true);
-
-        $this->json('PUT', '/incident/1', [
-            'title' => 'TestCase',
-            'description' => 'Descrição TestCase',
-            'criticalId' => 1,
-            'typeId' => 1,
-            'statusId' => 1,
-        ], [
-            'Content-Type' => 'application/json',
-            'token' => $responseToken['data']['token'],
-        ])->seeJson([
-            'action' => 3,
-        ]);
-    }
-
-    public function testShouldBeUpdateIncident()
-    {
-        $this->json('POST', '/token', [], [
-            'Content-Type' => 'application/json'
-        ]);
-        $responseToken = json_decode($this->response->getContent(), true);
-        $this->json('POST', '/account/login', [
-            'username' => 'admin',
-            'pass' => 'admin'
-        ], [
-            'Content-Type' => 'application/json',
-            'token' => $responseToken['data']['token'],
-        ]);
-
-        $this->json('PUT', '/incident/1', [
-            'title' => 'TestCase',
-            'description' => 'Descrição TestCase',
-            'criticalId' => 1,
-            'typeId' => 1,
-            'statusId' => 1,
-        ], [
-            'Content-Type' => 'application/json',
-            'token' => $responseToken['data']['token'],
-        ])->seeJson([
-            'action' => 0,
-        ]);
-    }
-
 
 }
